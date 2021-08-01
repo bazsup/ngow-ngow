3b60ceb05419c43c38e79d9a188bc039a19ac295
bazsup <bas@odds.team>
2021-08-01 12:19:23 +0700

    add random image

--- backend/messages.ts
+++ backend/messages.ts
@@ -13,6 +13,7 @@ export default {
     ],
     photos: [
         {url: 'https://i.pinimg.com/originals/52/86/d2/5286d29362b6ff12fbbe2431291784f0.jpg'},
-        {url: 'https://scontent.fbkk22-4.fna.fbcdn.net/v/t1.6435-9/139674546_3566975153415965_3696836067679643158_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=730e14&_nc_ohc=LXbU-4xM-osAX_mZYJC&_nc_ht=scontent.fbkk22-4.fna&oh=793eab8d10eb3092c6914eb6685fe5b4&oe=612B60CD'}
+        {url: 'https://scontent.fbkk22-4.fna.fbcdn.net/v/t1.6435-9/139674546_3566975153415965_3696836067679643158_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=730e14&_nc_ohc=LXbU-4xM-osAX_mZYJC&_nc_ht=scontent.fbkk22-4.fna&oh=793eab8d10eb3092c6914eb6685fe5b4&oe=612B60CD'},
+        {url: 'https://www.tnnthailand.com/static/images/997caecc-1630-419b-a71c-88ddeaed3a2a.jpg'}
     ]
 }
